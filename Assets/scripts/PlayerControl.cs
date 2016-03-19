dee9749faa642ef69dbf48aa27c8bacba3700bda
aniis1987 <anis.feki.87@gmail.com>
2016-03-19 14:55:21 -0400

    test

---
+++ Assets/scripts/PlayerControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+    public Animation anim;
+    // Update is called once per frame
+    void Update () {
+        Movement();
+	}
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector2.up * 4 * Time.deltaTime);
+            
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector2.down * 4 * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * 4 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * 4 * Time.deltaTime);
+        }
+    }
+}
