d3be4bfdf76f362c722a7888a1fc05dd15daddfb
Taimoor <taimoorrana1@hotmail.com>
2016-03-19 05:44:31 -0400

    added scores

---
+++ Assets/Scripts/StudentAI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StudentAI : MonoBehaviour {
+	GameManager gameManager;
+	// Use this for initialization
+	void Start () {
+		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition += transform.forward * 0.25f * Time.deltaTime;
+
+	}
+
+	void OnCollisionEnter(Collision coll) {
+        if (coll.gameObject.tag == "Wire"){
+        	gameManager.reducePoint();
+        	Debug.Log(gameManager.getPoints());
+            Destroy(gameObject); 
+        }  
+    }
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+	GameManager gameManager;
+	// Use this for initialization
+	void Start () {
+		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Text>().text = gameManager.getPoints().ToString();
+	}
+}
