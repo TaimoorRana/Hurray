babcd9e309a96cdab37f154281b8baa08ef0ea80
Taimoor <taimoorrana1@hotmail.com>
2016-03-19 04:19:39 -0400

    basic player movement with basic AI

---
+++ Assets/Scripts/GuardPlayer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardPlayer : MonoBehaviour {
+	float moveValVertical;
+	float moveValHorizontal;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+	}
+
+	void Move(){
+		moveValVertical = Input.GetAxis("Vertical");
+		moveValHorizontal = Input.GetAxis("Horizontal");
+		if(Input.GetKey(KeyCode.W)){
+			transform.localPosition += transform.forward * 0.5f * Time.deltaTime;
+		}
+		if(Input.GetKey(KeyCode.S)){
+			transform.localPosition += -transform.forward * 0.5f * Time.deltaTime;
+		}
+		if(Input.GetKey(KeyCode.A)){
+			transform.localPosition += -new Vector3(1,0,0) * 0.5f * Time.deltaTime;
+		}
+		if(Input.GetKey(KeyCode.D)){
+			transform.localPosition += new Vector3(1,0,0) * 0.5f * Time.deltaTime;
+		}
+	}
+
+	void OnCollisionEnter(Collision coll) {
+        if (coll.gameObject.tag == "Wire"){
+            Destroy(coll.gameObject); 
+        }  
+    }
+}
